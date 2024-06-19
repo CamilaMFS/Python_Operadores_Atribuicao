b95639912f7c250c36ee6e6aefd64a2965340336
Camila Marques <mfs.cami@gmail.com>
2024-06-18 22:25:00 -0300

    Add files via upload

---
+++ class6.py
@@ -0,0 +1,20 @@
+saldo = 500
+print(saldo)
+
+saldo = 200
+print(saldo)
+
+saldo += 10
+print(saldo)
+
+saldo -= 5
+print(saldo)
+
+saldo /= 2
+print(saldo)
+
+saldo //= 2
+print(saldo)
+
+saldo *= 10
+print(saldo)
